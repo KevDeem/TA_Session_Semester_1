5ae5b2f070c47a85fd05a584eb86a4f9d2b24ad8
Kevin Dimas <kevin@Kevins-MacBook-Pro.local>
2018-09-19 10:44:54 +0700

    TA ASSIGNMENT 1

---
+++ Triangle Assignment.py
@@ -0,0 +1,48 @@
+number = int(input("Input number here: "))
+
+y=1
+while y < number:
+    print("*" * y)
+    y = y + 1
+
+print("")
+
+y=1
+while y < number:
+    print(" " * (number - y) + "*" * (y))
+    y = y + 1
+
+print("")
+
+y = 1
+while y< number:
+    print(" " * y + "*" * (number - y))
+    y = y + 1
+
+print("")
+
+y = 1
+while y < number:
+    print("*" * (number - y))
+    y = y + 1
+
+print("")
+
+y = 1
+while y < number:
+    print(" " * (number - y) + "*" * (y + (y-1)))
+    y = y + 1
+
+print("")
+
+y = 1
+while y < number:
+    print(" " * (number - y) + "*" * (y + (y-1)))
+    y = y + 1
+
+x = 1
+while x < number:
+    print(" " * (x) + "*" * (number - x ) + ("*" * (number - x - 1)))
+    x = x + 1
+    if x == y:
+         break
