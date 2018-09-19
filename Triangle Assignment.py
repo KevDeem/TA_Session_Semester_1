234e9b2c0d020b937634f823a1def65a8638fc5c
Kevin Dimas <kevin@Kevins-MacBook-Pro.local>
2018-09-19 11:53:05 +0700

    TA ASSIGNMENT 1

--- Triangle Assignment.py
+++ Triangle Assignment.py
@@ -30,17 +30,17 @@ print("")
 
 y = 1
 while y < number:
-    print(" " * (number - y - 1) + "*" * (y + (y-1)))
+    print(" " * (number - y - 1) + "*" * (y + (y - 1)))
     y = y + 1
 
 print("")
 
 y = 1
 while y < number:
-    print(" " * (number - y-1) + "*" * (y + (y-1)))
+    print(" " * (number - y - 1) + "*" * (y + (y - 1)))
     y = y + 1
 
 x = 2
 while x < number:
-    print(" " * (x-1) + "*" * (number - x)+ ("*" * (number - x - 1)))
+    print(" " * (x - 1) + "*" * (number - x)+ ("*" * (number - x - 1)))
     x = x + 1
